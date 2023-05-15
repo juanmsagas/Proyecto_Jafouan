c1bc276885911c379bfc964664ff6fcbe7b78266
angie <juanmolinasagastume@gmail.com>
2023-05-15 14:55:22 -0600

    as

--- Proyecto_Jafouan_FrontEnd/src/components/header/AppHeaderDropdown.js
+++ Proyecto_Jafouan_FrontEnd/src/components/header/AppHeaderDropdown.js
@@ -25,6 +25,14 @@ import CIcon from '@coreui/icons-react'
 import avatar8 from './../../assets/images/avatars/avatar.png'
 
 const AppHeaderDropdown = () => {
+
+  const limpiarSesiones = () => {
+    sessionStorage.clear();
+  
+    // Redirige a la página de inicio de sesión u otra página deseada
+    window.location.href = "/";
+  };
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
@@ -33,10 +41,10 @@ const AppHeaderDropdown = () => {
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Cuenta</CDropdownHeader>
         <CDropdownDivider />
-        <CDropdownItem href="#">
-          <CIcon icon={cilLockLocked} className="me-2" />
-          Cerrar Sesión
-        </CDropdownItem>
+        <CDropdownItem href="#" onClick={limpiarSesiones}>
+  <CIcon icon={cilLockLocked} className="me-2" />
+  Cerrar Sesión
+</CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
   )
